28d1ac96aca21523052a7f6a6a8febcefed1939e
Javier Encina <encinajavier7@gmail.com>
2019-04-23 22:11:32 -0300

    Singleton

---
+++ Singleton/CSingleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    //Esta tecnica de singleton funciona cuando no usamos concurrencia/paralelismo
+    class CSingleton
+    {
+        //Aqui guardamos la únicxa instancia que va a existir
+        private static CSingleton instancia;
+
+        //Datos propios de la clase 
+        private string nombre;
+        private int edad;
+
+        //Creamos el constructor privado
+        private CSingleton()
+        {
+            nombre = "Sin asignar";
+            edad = 99;
+        }
+
+        public static CSingleton ObtenInstancia()
+        {
+            //Verificamos si no existe la instancia
+            if(instancia==null)
+            {
+                //Si no existe, instanciamos
+                instancia = new CSingleton();
+                Console.Write("---Instancia creada por primera vez");
+            }
+
+            //REgresamos la instancia
+            return instancia;
+        }
+
+        //Aqui metodos propios de la clase
+        public override string ToString()
+        {
+            return string.Format("La persona {0}, tiene edad de {1}",nombre, edad);
+        }
+
+        public void PonerDatos(string pNombre, int edad)
+        {
+            this.nombre = pNombre;
+            this.edad = edad;
+        }
+
+        //Esto representa cualquier otro objeto
+        public void AlgunProceso()
+        {
+            Console.WriteLine("{0} esta trabajando en algo", nombre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //No se puede obtener la instancia directamente
+            //CSingleton single = new CSingleton();
+
+            //Obtenemos la instancia, se crea por primera vez
+            CSingleton uno = CSingleton.ObtenInstancia();
+
+            //Hacemos algo con la instancia
+            uno.PonerDatos("Ana", 21);
+            uno.AlgunProceso();
+            Console.WriteLine(uno);
+            Console.WriteLine("-----");
+
+            //Obtenemos la instancia
+            CSingleton dos = CSingleton.ObtenInstancia();
+
+            //Comprobamos que es la misma instancia
+            //Si lo es tendra el mismo estado
+            Console.WriteLine(dos);
+            Console.ReadKey();
+        }
+    }
+}
